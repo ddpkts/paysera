56bea93de89a56dfc38649fea753ec18607ccb05
Tomas Kvietkauskas <tomas@kvietkauskas.lt>
2014-11-05 22:47:32 +0200

    module init

---
+++ ddpkts/paysera/README.md
@@ -0,0 +1,14 @@
+# Paysera
+
+Paysera/Mokėjimai.lt OXID payment module
+
+## Installation
+
+* Activate module in administration area
+* Configure module settings (project id and password are required)
+* Configure shop payment settings (assign users, countries, shipping methods and cost rules)
+
+## Uninstall
+
+* Deactivate module
+* Remove Paysera payment method
---
+++ ddpkts/paysera/metadata.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.1';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'          => 'paysera',
+    'title'       => "Paysera",
+    'description' => 'Paysera/Mokėjimai.lt OXID payment module',
+    'version'     => '1.0.0',
+    'author'      => 'ddpkts',
+    'email'       => 'hallo@ddpkts.com',
+    'extend'      => array(
+    ),
+    'files'       => array(
+    ),
+    'templates'   => array(
+    ),
+    'blocks'      => array(
+    ),
+    'settings'    => array(
+    ),
+    'events'      => array(
+    ),
+);
